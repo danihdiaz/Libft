3cccc2308e59ba560e6b69a8188e5aec6cd51249
Daniel Hontanilla Diaz <dhontani@c2r16s4.42madrid.com>
2026-01-19 17:15:05 +0100

    g

---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhontani <dhontani@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-01-19 15:47:24 by dhontani          #+#    #+#             */
+/*   Updated: 2026-01-19 15:47:24 by dhontani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *d = (unsigned char *)dest;
+    unsigned char *s = (unsigned char *)src;
+    size_t = i;
+
+    i = 0;
+    while (i < n)
+    
+}
